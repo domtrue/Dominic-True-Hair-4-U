d1ea789a23fa217f5fe131ca91e100e80fe3b900
domtrue <62486205+domtrue@users.noreply.github.com>
2024-05-01 10:50:11 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dominic-True-Hair-4-U
+ 
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hair 4 U</title>
+    <link rel="stylesheet" href=<"style.css">
+    </head>
+    
+    <body>
+        
+        <section id="header">
+            <a href="#"><img src="img/logo.png" class="logo" alt=""</a>
+                
+            <div>
+               <ul id="navbar">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Promotions</a></li>
+                    <li><a href="">Shop</a></li>
+                    <li><a href="">Book Appointment</a></li>
+                   <li><a href="">Hair Services</a></li>
+                   <li><a href="">Gift Vouchers</a></li>
+                   <li><a href="">About</a></li>
+            </div>
+        </section>
+    
+    
+        <script src="script.js"></script>
+    </body>
+    
+</html>
