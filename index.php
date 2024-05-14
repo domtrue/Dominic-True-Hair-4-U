9bc69b56b6a45539558f1c87ceead2e0a54385f3
domtrue <62486205+domtrue@users.noreply.github.com>
2024-05-14 15:43:30 +1200

    Colour gradient added
    Added colour gradient for homepage body. Text in navbar is now horizontally aligned. Am just exploring how to get the logo to stay centered and adapt to wider (higher resolution) displays.

--- index.php
+++ index.php
@@ -8,30 +8,36 @@
 
 </head>
 <body>
-
-<nav>
-    <div class="left-menu">
-        <ul>
+    <nav class="navbar">
+        <div class="navbar_ _left">
             <li><a href="promotions.php">Promotions</a></li>
             <li><a href="shop.php">Shop</a></li>
             <li><a href="book_appointment.php">Book Appointment</a></li>
-        </ul>
-    
-    <nav>
-    <a href="index.php" class="logo-container">
+        </div>
+        <div class="navbar_ _logo">
         <img class="logo-img" src="img\logo.png" alt="Logo">
-    </a>
-    <nav>
-    
-     <div class="right-menu">  
-        <ul>
+        </div>
+        <div class="navbar_ _ right">
             <li><a href="hair_services.php">Hair Services</a></li>
             <li><a href="gift_vouchers.php">Gift Vouchers</a></li>
             <li><a href="about.php">About</a></li>
-        </ul>
-        
-   
-    </div>
+        </div>
     </nav>
+
+    <div class="content">
+
+    </div>
+
+    <style>
+        .content {
+            background: linear-gradient(135deg, #8e2de2, #4a00e0);
+            height: calc(100vh - 60px); 
+        }
+
+    </style>
+
+    <footer class="footer">
+        <p>&copy; 2024 Your Company</p>
+    </footer>
 </body>
 </html>
