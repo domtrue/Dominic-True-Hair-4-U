e5a7ecf6f3aa38a7295501f11f7d0914064b8977
domtrue <62486205+domtrue@users.noreply.github.com>
2024-05-20 14:57:42 +1200

    Completed footer
    Social media icons, copyright notice and the business' address have all been aligned properly. Social media icons on the left, copyright notice in the center and the business' address on the right.

--- index.php
+++ index.php
@@ -48,8 +48,18 @@
 
     </style>
 
-    <footer class="footer">
-        <p>&copy; Triple Five Enterprises Limited</p>
-    </footer>
+    <footer>
+        <div class="social-icons">
+            <a href="https://www.facebook.com/hair4ubulls/" class="social-icon"><img src="img\facebook-logo.png" alt="Facebook"></a>
+            <a href="https://www.instagram.com/truehairdressing/" class="social-icon"><img src="img\instagram-logo.png" alt="Instagram"></a>
+        </div>
+        <div class="copyright">
+            &copy; Triple Five Enterprises Limited
+        </div>
+        <div class="address">
+            68 High Street, Bulls 4818
+        </div>
+</footer>
+
 </body>
 </html>
