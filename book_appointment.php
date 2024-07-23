afda4d3f5b4e1649241fdb00581798e77ffc888f
domtrue <62486205+domtrue@users.noreply.github.com>
2024-07-23 20:36:47 +1200

    No more blank pages
    Have carried over basic HTML script to all pages of the site so there are no more blank pages

---
+++ book_appointment.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hair 4 U</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        .content {
+            background: linear-gradient(135deg, #8e2de2, #4a00e0);
+            height: calc(100vh - 60px); /* Full viewport height minus 60px */
+            min-height: 100px; /* Minimum height to ensure background is visible */
+        }
+  </style>
+</head>
+<body>
+<?php include 'header.php';?>
+<div class="content">
+        
+    </div>
+
+<?php include 'footer.php';?>
+    <script src="script.js"></script>
+</body>
+</html>
